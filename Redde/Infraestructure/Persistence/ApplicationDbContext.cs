07c9096f4bd69ce4a461a5610cb0e27a439eaa8a
Seventty <zeroseventty@gmail.com>
2025-04-28 09:47:15 -0400

    ApplicationDbContext created and linked into program.cs

---
+++ Redde/Infraestructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Redde.Domain.Entities;
+
+namespace Redde.Infraestructure.Persistence
+
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {}
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Compay> Companies {  get; set; }
+        public DbSet<Role> Roles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>()
+                .HasOne(u => u.Name)
+                .withMany()
+                .HasForeignKey(u => u.RoleId);
+
+            modelBuilder.Entity<User>()
+                .HasOne(u => u.CompanyId)
+                .WithMany()
+                .HasForeignKey(u => u.CompanyId);
+
+            modelBuilder.Entity<Company>()
+                .HasOne(c => c.Owner)
+                .WithMany()
+                .HasForeignKey(c => c.OwnerId);
+
+
+        }
+    }
+}
