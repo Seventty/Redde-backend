d769779381d5e3f91fc9c6e8fcbd0b2f7e8534e0
Seventty <zeroseventty@gmail.com>
2025-04-28 18:43:56 -0400

    Add Company, Role, and User classes to domain entities
    This commit introduces three new classes: `Company`, `Role`, and `User` in the `Redde.Domain.Entities` namespace. Each class is designed with properties relevant to their respective entities. The `Company` class includes properties for `Id`, `Name`, `Address`, `Phone`, and a reference to an `Owner` of type `User`. The `Role` class contains properties for `Id` and `Name`. The `User` class features properties for `Id`, `Name`, `Email`, `Password`, and references to a `Role` and an optional `Company`. The use of `null!` indicates that these properties are expected to be initialized and cannot be null.

---
+++ Redde/Domain/Entities/Company.cs
@@ -0,0 +1,14 @@
+﻿namespace Redde.Domain.Entities
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Address { get; set; } = null!;
+        public string Phone { get; set; } = null!;
+        public int OwnerId { get; set; }
+
+        public User Owner { get; set; } = null!;
+
+    }
+}
---
+++ Redde/Domain/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿namespace Redde.Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public int RoleId { get; set; }
+        public int CompanyId { get; set; }
+        public Role Role { get; set; } = null!;
+        public Company? Company { get; set; }
+    }
+}
