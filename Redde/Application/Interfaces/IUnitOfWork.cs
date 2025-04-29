6d7436b17dc51d8ce6e7d28abb2553975cb9e995
Seventty <zeroseventty@gmail.com>
2025-04-28 22:17:50 -0400

    Add IUnitOfWork interface and UnitOfWork implementation
    Introduces the `IUnitOfWork` interface to define a unit of work pattern with repository properties for `User`, `Company`, and `Role` entities. Implements the `UnitOfWork` class, which initializes these repositories and provides methods for saving changes asynchronously and disposing of resources.

---
+++ Redde/Application/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using Redde.Domain.Entities;
+
+namespace Redde.Application.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<User> Users { get; }
+        IRepository<Company> Companies { get; }
+        IRepository<Role> Roles { get; }
+
+        Task<int> SaveChangesAsync();
+    }
+}
